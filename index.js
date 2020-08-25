5d410eea72225bf4d958745e066833984a7633f6
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2020-08-25 17:36:31 -0400

    primeira versão ate aula 28

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+//estou informando para o express usar o ejs como view engine(renderizador de html)
+app.set('view engine','ejs');
+app.use(express.static('public')); //definindo arquivos staticos (explicação na aula 28 secao 05)
+
+app.get("/:nome/:lang", function(req, resp){
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var msg = true;
+    var produtos =  [ 
+        {nome: "doritos", valor:3.14},
+        {nome: "coca-cola", valor:5.50},
+        {nome: "leite", valor:3}
+    ]
+
+    resp.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "guia do programador",
+        msg: exibirMsg,
+        produtos: produtos
+    }); //render = renderizar o html=ejs (não precisa colocar o diretorio completo (views/index),pois automaticamente ele olha dentro da pasta views)
+});
+
+app.listen(8080,()=> { console.log("app rodando!")});

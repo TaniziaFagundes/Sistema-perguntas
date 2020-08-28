b4555791611fc351a953d6f53873fe3df8646a42
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2020-08-27 21:26:24 -0400

    insenrindo bootstrap e partials

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app = express()
 app.set('view engine','ejs');
 app.use(express.static('public')); //definindo arquivos staticos (explicação na aula 28 secao 05)
 
-app.get("/:nome/:lang", function(req, resp){
+app.get("/", function(req, resp){
 
-    resp.render("index",{
-        
-    }); //render = renderizar o html=ejs (não precisa colocar o diretorio completo (views/index),pois automaticamente ele olha dentro da pasta views)
+    resp.render("index"); //render = renderizar o html=ejs (não precisa colocar o diretorio completo (views/index),pois automaticamente ele olha dentro da pasta views)
 });
+
+app.get("/perguntar", (req, resp) => {
+    resp.render("perguntar");
+})
 
 app.listen(8080,()=> { console.log("app rodando!")});
